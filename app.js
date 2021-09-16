f4d24798c64bee1d47dc9e4f021aff061e78dcad
musab90 <adem.musab1@gmail.com>
2021-09-16 18:59:07 +0300

    covid-19 data

---
+++ app.js
@@ -0,0 +1,61 @@
+//show cart
+
+
+const xlabel = [];
+const ylabel = [];
+async function showChart(){
+
+await getData();
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: xlabel,
+        datasets: [{
+            label: 'covid-19 Data',
+            data: ylabel,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+};
+showChart()
+
+// fetch data
+getData();
+async function  getData(){
+    const api =  'https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true';
+    const response = await fetch(api);
+    const datas =  await response.json();
+    datas.forEach(data => {
+        const {country, infected} = data;
+        xlabel.push(country);
+        ylabel.push(infected);
+    });
+}
+
+
